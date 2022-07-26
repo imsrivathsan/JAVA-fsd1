1a8500c8a9357ddb0015c63ce2eedaf858b25070
Srivathsan <srvthsns@gmail.com>
2022-07-26 10:13:25 +0530

    First Commit
    Uploading All files

---
+++ phase1/src/PracticeProject/TypeCasting.java
@@ -0,0 +1,29 @@
+package PracticeProject;
+import java.util.*;
+public class TypeCasting {
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n1;
+		System.out.print("Enter the value of n1: ");
+		n1=sc.nextInt();
+		double d,d1;
+		d=n1;
+		System.out.println("Implicit TypeCasting");
+		System.out.println("After Typecasting : "+ d);
+		d1=n1+5.6;
+		System.out.println("After Typecasting : "+ d1);
+		System.out.println("Explicit TypeCasting");
+		double d2;
+		System.out.print("Enetr the value of d2 : ");
+		d2=sc.nextDouble();
+		int i1=(int)d2;
+		System.out.println("After TypeCasting : "+ i1);
+		int v;
+		System.out.print("Enter the value of v: ");
+		v=sc.nextInt();
+		char c=(char)v;
+		System.out.println("After Typecasting : "+ c);
+		
+	}
+}
