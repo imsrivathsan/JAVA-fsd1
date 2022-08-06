ac45ff413b522ea81cba8b4c72988bb171d627a9
Srivathsan <srvthsns@gmail.com>
2022-08-06 17:40:08 +0530

    Sixth commit

--- phase1/src/PracticeProject/LongestIncreasingSubsequence.java
+++ phase1/src/PracticeProject/LongestIncreasingSubsequence.java
@@ -26,7 +26,10 @@ public class LongestIncreasingSubsequence {
 	{
 		int a[]={7,1,4,8,11,2,14,3};
 		int b[]={10, 22, 9, 33, 21, 50, 41, 60, 80};
+		System.out.println("LongestIncreasingSubsequence of the given array");
+		System.out.print("array A : ");
 		System.out.println(LongestIncreasingSubsequence.LisLength(a));
+		System.out.print("array B : ");
 		System.out.println(LongestIncreasingSubsequence.LisLength(b));
 		
 	}
