023387b16256bb6b9689db6b55180316fa55584e
Srivathsan <srvthsns@gmail.com>
2022-08-10 11:28:06 +0530

    first commit

---
+++ ValidationofLoginLogout/src/LoginServlet.java
@@ -0,0 +1,44 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String  userid = request.getParameter("userid");
+		String password = request.getParameter("password");
+		HttpSession session = request.getSession();
+		if(password.equals("admin"))
+		{
+			session.setAttribute("userid", userid);
+			response.sendRedirect("./DashBoardServlet");
+		}
+		else
+		{
+			response.sendRedirect("login.html");
+		}
+				
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
